4a2b073277d30c80a05cfca8accd342e96122988
CesarMoschini <cesar_moschini@hotmail.com>
2022-04-08 19:59:50 -0300

    Entrega de API

--- PreentregaProyecto2_Moschini/js/contact.js
+++ PreentregaProyecto2_Moschini/js/contact.js
@@ -10,3 +10,19 @@ function enviarForm(){
     console.log(`Mail: ${mailForm}`);
     console.log(`El comentario hecho es: ${comentarioForm}`);
 };
+
+var contenido = document.querySelector('#contenido')
+function traer() {
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.results['0'])
+        contenido.innerHTML = `
+        <img src="${data.results['0'].picture.large}" width="100px" class="img-fluid rounded-circle"> 
+        <h3> ${data.results['0'].name.first} ${data.results['0'].name.last}</h3>
+        <h4> ${data.results['0'].location.city}, ${data.results['0'].location.state}</h4>
+        `
+    })
+}
+
+setInterval('traer()',2000);
